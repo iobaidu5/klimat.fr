979008a1bc21d99c06120c49223126e132754bf8
iobaidu5 <i.obaidu5@gmail.com>
2023-09-21 11:58:05 +0500

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -124,3 +124,18 @@ video.addEventListener("playing", function () {
 video.addEventListener("pause", function () {
 	circlePlayButton.style.opacity = 1;
 });
+
+
+$('.counter-count').each(function () {
+  $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+  }, {
+
+      //chnage count up speed here
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
